73249335d5363637e075e4da6b60c944b1818bef
Bagiwka <sagivka2006@gmail.com>
2023-11-30 22:49:52 +0100

    I tried

---
+++ LCInstantFloods/LCInstantFloods/Patches/FloodWeatherPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace LCInstantFloods.Patches
+{
+    [HarmonyPatch(typeof(FloodWeather))]
+    internal class FloodWeatherPatch
+    {
+        private static FieldInfo floodLevelOffsetField = typeof(FloodWeather).GetField("floodLevelOffset", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        [HarmonyPatch("OnGlobalTimeSync")]
+        [HarmonyPrefix]
+        static void Prefix(FloodWeather __instance)
+        {
+            // Access floodLevelOffset using reflection
+            float currentFloodLevelOffset = (float)floodLevelOffsetField.GetValue(__instance);
+
+            // Modify floodLevelOffset with a multiplier to make it change faster
+            float multiplier = 10.0f; // Adjust this multiplier as needed
+            float modifiedFloodLevelOffset = Mathf.Clamp(TimeOfDay.Instance.globalTime / 1f, 0f, 100f) * multiplier * (float)TimeOfDay.Instance.currentWeatherVariable2;
+
+            floodLevelOffsetField.SetValue(__instance, modifiedFloodLevelOffset);
+        }
+    }
+}
---
+++ LCInstantFloods/LCInstantFloods/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCInstantFloods.Patches;
+
+namespace LCInstantFloods
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LCInfiniteHealthModBase : BaseUnityPlugin
+    {
+
+        private const string modGUID = "Bagiwka.LCInstantFloods";
+        private const string modName = "LCInstantFloods";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LCInfiniteHealthModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Time to drown!");
+
+            harmony.PatchAll(typeof(LCInfiniteHealthModBase));
+            harmony.PatchAll(typeof(FloodWeatherPatch));
+
+
+        }
+
+    }
+}
